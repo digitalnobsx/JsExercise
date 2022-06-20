c3d3f931b252af4184d369faa7f5f935d2d58d39
digitalnobsx <josephitoro23@gmail.com>
2022-06-20 22:02:53 +0100

    Add basic front-end

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const name = 'Itoro';
-const height = 6.2;
+const height = "6'2";
 const country = 'Nigeria';
 
-console.log(`My name is Itoro ${name}. I am ${height} and I am from ${country}.`);
+document.write(`My name is ${name}. I am ${height} and I am from ${country}.`);
